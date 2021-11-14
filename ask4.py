0ecc4874e14cec922cbb69d4e35c88d44da5c1c6
nikolastas <54937147+nikolastas@users.noreply.github.com>
2021-11-14 16:13:55 +0200

    works for d=1

---
+++ ask4.py
@@ -0,0 +1,28 @@
+import numpy as np
+import array
+
+d=1
+S=1
+temp=1
+change=0
+a=[2,2,2,4,4]
+for i  in range(0,len(a)-1):
+   print("compare1= ",a[i] ,"compare2= ",a[i+1])
+   #print((a[i] > a[i+1]+d),(a[i] <= a[i+1]+d))
+   if (a[i+1] < a[i]+d):
+       temp+=1
+       change=1
+       print("1")
+   elif (a[i+1] >= a[i]+d) and temp!=0:
+       temp-=1
+       print("2a")
+       if change==1:
+           temp=1
+           change=0
+           print("2b")
+        
+   if temp>S:
+        S+=1
+        print("3")
+
+print("temp= ",temp," S= ",S)
