cff16fb5f43a7ca79bf2be7dd2a65e6622d7aa2b
nikolastas <54937147+nikolastas@users.noreply.github.com>
2021-11-14 16:36:55 +0200

    works only for d=2

--- ask4.py
+++ ask4.py
@@ -9,19 +9,21 @@ a=[2,2,2,4,4]
 for i  in range(0,len(a)-1):
    print("compare1= ",a[i] ,"compare2= ",a[i+1])
    #print((a[i] > a[i+1]+d),(a[i] <= a[i+1]+d))
-   if (a[i+1] < a[i]+d):
+   if (a[i+1] == a[i]):
        temp+=1
-       change=1
+       #change=1
        print("1")
-   elif (a[i+1] >= a[i]+d) and temp!=0:
+   else: 
+      temp=1
+   """elif (a[i+1] >= a[i]+d) and temp!=0:
        temp-=1
        print("2a")
        if change==1:
            temp=1
            change=0
-           print("2b")
+           print("2b")"""
         
-   if temp>S:
+   if temp>d:
         S+=1
         print("3")
 
