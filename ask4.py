3be5d2890f387cb3a9aeaab8057e67c27c7fe48a
nikolastas <54937147+nikolastas@users.noreply.github.com>
2021-11-15 21:32:58 +0200

    first commit

--- ask4.py
+++ ask4.py
@@ -1,11 +1,11 @@
 import numpy as np
 import array
 
-d=4
+d=2
 S=1
 temp=1
 change=0
-a=[2,2,2,4,4,5,6,7,8,8,8,8,8,8,8,8,8,8,8,8]
+a=[2,2,2,2,2,2,3,4,4,4,4,4]
 for i  in range(0,len(a)-1):
    print("compare1= ",a[i] ,"compare2= ",a[i+1])
    #print((a[i] > a[i+1]+d),(a[i] <= a[i+1]+d))
