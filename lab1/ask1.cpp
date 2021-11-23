4d783ad12f0493bad1909a75c7d059d00d2010b8
nikolastas <54937147+nikolastas@users.noreply.github.com>
2021-11-23 12:52:54 +0200

    first lines

--- lab1/ask1.cpp
+++ lab1/ask1.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <stdio.h> 
+#include <fstream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int max(int a, int b){
+    if(a>b){
+        return b;
+    }
+    else {
+        return a;
+    }
+}
+
+int main(int argc, char *argv[]){
 
 
-int maint(){
-
-
+    int n, m;
     
+    cout<<n<<m<<endl;
+    fstream myfile(argv[1], ios_base::in);
+    myfile>>n>>m;
+    vector<vector<int>,int > portals(n+1,(vector<vector<int>>(n+2),0));
+    cout << n <<m;
+    for (int i=0;i<m;i++){
+        myfile>>temp>>portals[i][1];
+        myfile>>portals[i+1];
+        cout<<portals[i][1];
+    }
 }
