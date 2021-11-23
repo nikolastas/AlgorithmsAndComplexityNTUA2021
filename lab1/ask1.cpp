6f49898842280fe1672c3e06055c6b5e53f0d4ae
nikolastas <54937147+nikolastas@users.noreply.github.com>
2021-11-23 22:25:07 +0200

    found hash but differend approuch need it

--- lab1/ask1.cpp
+++ lab1/ask1.cpp
@@ -5,25 +5,34 @@
 #include <algorithm>
 #include <map>
 #include <unordered_map>
-#include <contr
 
-using namespace std;
 
-template<typename T, typename A> 
-  std::size_t hash_value(std::vector<T, A> const& val);
-struct KeyHasher
-{
-  std::size_t operator()(const Key& k) const
-  {
-    using std::size_t;
-    using std::hash;
-    using std::string;
 
-    return ((hash<string>()(k.first)
-             ^ (hash<string>()(k.second) << 1)) >> 1)
-             ^ (hash<int>()(k.third) << 1);
-  }
+
+namespace std {
+struct Key {
+    vector<int> first;
+    
 };
+struct KeyEqual {
+ bool operator()(const Key& lhs, const Key& rhs) const
+ {
+    return lhs.first == rhs.first ;
+ }
+};
+struct KeyHash{
+        std::size_t operator()(const Key& k) const {
+        std::size_t seed = k.first.size();
+        for(auto& i : k.first) {
+            seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        }
+        
+        return (seed);
+
+    }
+};
+}
+using namespace std;
 
 int max(int a, int b){
     if(a>b){
@@ -49,13 +58,26 @@ void printvector(vector<int> pair){
                 }
     cout<<endl;
 }
+
+void printmap2(unordered_map<Key,int,KeyHash,KeyEqual > states){
+    for (auto s = states.begin(); s != states.end(); s++) {
+                printvector(s->first.first);
+                
+            
+            cout<<endl;
+            }
+}
+
+
+
+
 void swap(int a, int b, vector<int>& current){
     int temp=current[a-1];
     current[a-1]=current[b-1];
     current[b-1]=temp;
 
 }
-void goThroughPortal(vector<int> current_state,vector<int> portals,unordered_map<vector<int>,int>& states){
+void goThroughPortal(vector<int> current_state,vector<int> portals,unordered_map<Key,int,KeyHash,KeyEqual>& states){
     //cout<<"portal started"<<endl;
     //printvector(current_state);
     int portal1,portal2,cost;
@@ -65,7 +87,10 @@ void goThroughPortal(vector<int> current_state,vector<int> portals,unordered_map
     cost=portals[2];
     swap(portal1,portal2,current_state);
     //printvector(current_state);
-    states.insert(pair< vector<int>,int > ( current_state,cost));
+    Key k;
+    k.first=current_state;
+
+    states.emplace(k,cost);
     //cout<<"end portal"<<endl;
 }
 
@@ -73,48 +98,60 @@ int main(int argc, char *argv[]){
 
 
     int n, m;
-    
+    bool print=false;
+    if(argc>=3){
+        print=true;
+    }
     
     fstream myfile(argv[1], ios_base::in);
     myfile>>n>>m;
-    vector<vector<int> > portals(n+1,vector<int>(0));
+    vector<vector<int> > portals(m+1,vector<int>());
     vector<int> initial_state;
-    cout <<"N="<< n<<" M=" <<m<<endl;
+    if(print)
+        cout <<"N="<< n<<" M=" <<m<<endl;
     int temp1,temp2,temp3;
     
+
     for (int i=0;i<n;i++){
         myfile>>temp1;
         initial_state.push_back(temp1);
     }
-    unordered_map<vector<int>,int,_Hash_impl=ha> states; 
-    states.insert(pair< vector<int>,int > (initial_state,int));
+    if(print)
+        {cout<<"initial state is ";
+        printvector(initial_state);
+        cout<<endl;}
+    Key newk;
+    newk.first=initial_state;
+    unordered_map<Key,int,KeyHash,KeyEqual> states; 
+    states.emplace(newk,0);
+    int k=0;
     for (int i=0;i<m;i++){
         
         myfile>>temp1>>temp2>>temp3;
-        
+        k++;
         portals[i].push_back(temp1);
         portals[i].push_back(temp2);
         portals[i].push_back(temp3);
         
+    
         
         
     }
-    printvector(portals[0]);
-    printvector(portals[1]);
-    //cout<<"initial state is ";
-    //printvector(initial_state);
-    //cout<<endl;
+    if(print)
+        cout<<"ended inserting the portlas data"<<endl;
+    
     vector<int> temp;
     for(int j=0;j<m;j++){
         
         for (auto i = states.begin(); i != states.end(); i++){
             
-            goThroughPortal(i->first,portals[j],states);
-            
+            goThroughPortal(i->first.first,portals[j],states);
+            printmap2(states);
         }
         
     }
-    
+    if(print)
+        cout<<"ended goThroughPartals"<<endl;
     vector<int> final_state(n);
     generate( final_state.begin(), final_state.end(), [i=1]{
          static int i=1;
@@ -122,9 +159,9 @@ int main(int argc, char *argv[]){
      });
      printvector(final_state);
 
-    // if(states.find(0)!=states.end()){
-    //     cout<<"nice"<<endl;
+    //if(states.find(final_state)!=states.end()){
+    //     cout<<states[final_state];
     // }
-    cout<<"ended";
+    
     
 }
