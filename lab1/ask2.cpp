67b4336febbd76fee4debf013b553a27b2391e12
nikolastas <nikolas2000tas@gmail.com>
2021-11-29 12:56:06 +0200

    new version better hash fuinction and buckets

--- lab1/ask2.cpp
+++ lab1/ask2.cpp
@@ -9,7 +9,7 @@
 #include <tuple>
 #include <bits/stdc++.h>
 #include <list>
-
+using namespace std::chrono;
 using namespace std;
 
 // void printvector(vector<int> pair){
@@ -22,15 +22,35 @@ using namespace std;
 //     return (a+b)*(a+b+1)/2+b;
 // }
 
-
+struct modified_hash {
+ 
+    static uint64_t splitmix64(uint64_t x)
+    {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30))
+            * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27))
+            * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+ 
+    int operator()(uint64_t x) const
+    {
+        static const uint64_t random
+            = steady_clock::now()
+                  .time_since_epoch()
+                  .count();
+        return splitmix64(x + random);
+    }
+};
 
 
 
 
 int main(int argc, char *argv[]){
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
-    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // auto start = std::chrono::high_resolution_clock::now();
     int n, k;
     int sum=0;
     int p=0;
@@ -43,21 +63,23 @@ int main(int argc, char *argv[]){
     
     
     //scanf("%d %d",&n,&k);
-    fstream myfile(argv[1], ios_base::in);
+    // fstream myfile(argv[1], ios_base::in);
     
-    myfile>>n>>k;
-    // cin>>n>>k;
+    // myfile>>n>>k;
+    cin>>n>>k;
     vector<int> arr(n+1);
     
     for(int i=0;i<n;i++){
         //scanf("%d",&arr[i]);
-        myfile>>arr[i];
-        // cin>>arr[i];
+        // myfile>>arr[i];
+        cin>>arr[i];
     }
     
-    auto after_reading = std::chrono::high_resolution_clock::now();
+    // auto after_reading = std::chrono::high_resolution_clock::now();
     vector<vector<int>> sub_sums(2*n+1,vector<int>());
     unordered_map<int,int> mymap;
+    mymap.reserve(k); // RESERVING SPACE BEFOREHAND
+    mymap.max_load_factor(0.25);
 
     bool check_for_1_element=false;
     
@@ -66,7 +88,7 @@ int main(int argc, char *argv[]){
         //temp_result=999999;
         for(int j=i;j<n;j++){
             if(sum+arr[j]<=k){
-            sum+=arr[j];
+                sum+=arr[j];
             if(sum==k && j==i){
                 result=1;
                 check_for_1_element=true;
@@ -87,26 +109,29 @@ int main(int argc, char *argv[]){
                 break;
                 
             }
-            if(check_for_1_element){
-                break;
-            }
+            
 
         }
-    if(!(check_for_1_element)){
-    for(int k=0;k<sub_sums[i].size();k+=2){
-        //printf("new map insert for i=%d sum=%d map=%d\n",i,sub_sums[i][k],mymap[sub_sums[i][k]]);
-        if(mymap[sub_sums[i][k]]){
-            //printf("[%d] new possible map value =%d\n",mymap[sub_sums[i][k]],sub_sums[i][k+1]);
-            mymap[sub_sums[i][k]]=min(mymap[sub_sums[i][k]],sub_sums[i][k+1]);
+        if(check_for_1_element){
+                break;
+            }
+        else {
+            for(int k=0;k<sub_sums[i].size();k+=2){
+                int v = mymap[sub_sums[i][k]];
+                //printf("new map insert for i=%d sum=%d map=%d\n",i,sub_sums[i][k],mymap[sub_sums[i][k]]);
+                if(v){
+                    //printf("[%d] new possible map value =%d\n",mymap[sub_sums[i][k]],sub_sums[i][k+1]);
+                    mymap[sub_sums[i][k]]=min(v,sub_sums[i][k+1]);
+                    // mymap.replace(v,min(v,sub_sums[i][k+1]));
+                }
+                else{
+                    mymap[sub_sums[i][k]]=sub_sums[i][k+1];
+                }
+                
+            }
         }
-        else{
-            mymap[sub_sums[i][k]]=sub_sums[i][k+1];
-        }
-        
     }
-    }
-    }
-    auto pref = std::chrono::high_resolution_clock::now();
+    // auto pref = std::chrono::high_resolution_clock::now();
     
         
         
@@ -118,11 +143,11 @@ int main(int argc, char *argv[]){
         result=-1;
     }
     cout<<result<<endl;
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration_pref=std::chrono::duration_cast<std::chrono::milliseconds>(pref - start);
-    auto duration=std::chrono::duration_cast<std::chrono::milliseconds>(after_reading - start);
-    auto duration_end=std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    cout<<"after reading "<<duration.count() << endl;
-    cout<<"pref "<<duration_pref.count() << endl;    
-    cout<<"duration "<<duration_end.count() << endl;
+    // auto end = std::chrono::high_resolution_clock::now();
+    // auto duration_pref=std::chrono::duration_cast<std::chrono::milliseconds>(pref - start);
+    // auto duration=std::chrono::duration_cast<std::chrono::milliseconds>(after_reading - start);
+    // auto duration_end=std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    // cout<<"after reading "<<duration.count() << endl;
+    // cout<<"pref "<<duration_pref.count() << endl;    
+    // cout<<"duration "<<duration_end.count() << endl;
 }
