5250530e3b6fd4f5505b7617d0ad459cea53d8fe
nikolastas <54937147+nikolastas@users.noreply.github.com>
2021-12-02 19:57:39 +0200

    better version where the initialise array is used in heap and not in stack

--- ask2.cpp
+++ ask2.cpp
@@ -1,71 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <chrono>
 #include <stdio.h>
 #include <stdlib.h>
-#include <set>
-#include <unordered_map>
-#include <tuple>
 #include <bits/stdc++.h>
-#include <list>
-using namespace std::chrono;
 using namespace std;
 
-// void printvector(vector<int> pair){
-//     for(int i=0;i<pair.size();i++){
-//                     printf("%d ",pair[i]);
-//                 }
-//     cout<<endl;
-// }
-// int make2dto1d(int a, int b){
-//     return (a+b)*(a+b+1)/2+b;
-// }
-
-struct modified_hash {
- 
-    static uint64_t splitmix64(uint64_t x)
-    {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30))
-            * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27))
-            * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
- 
-    int operator()(uint64_t x) const
-    {
-        static const uint64_t random
-            = steady_clock::now()
-                  .time_since_epoch()
-                  .count();
-        return splitmix64(x + random);
-    }
-};
 
 
 
+
+long int *val ;
 
 int main(int argc, char *argv[]){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    // auto start = std::chrono::high_resolution_clock::now();
     int n;
     long int k;
     long int sum=0;
-    
-    // int temp_k;
-    // int temp_sum=0;
     int result=999999999;
-    //tuple<int,int> mytuple;
-    // int temp_result=0;
-
-    
-    
     //scanf("%d %d",&n,&k);
     fstream myfile(argv[1], ios_base::in);
-    
     myfile>>n>>k;
     // cin>>n>>k;
     vector< long int> arr(n+1);
@@ -73,35 +28,20 @@ int main(int argc, char *argv[]){
     for(int i=0;i<n;i++){
         //scanf("%d",&arr[i]);
         myfile>>arr[i];
-        
         // cin>>arr[i];
         
     }
     
-    // auto after_reading = std::chrono::high_resolution_clock::now();
     vector<vector<long int>> sub_sums(2*n+1,vector<long int>());
-    // unordered_map<int,int> mymap;
     
-    long int *val ;
-    int *thesi;
-    // int *i_thesi;
     size_t size=sizeof(long long int)*(k+1);
-    size_t size_thesi=sizeof(long int )*(k+1);
-    // size_t i_size_thesi=sizeof(long int )*(k+1);
     val = (long int *) malloc(size);
-    thesi=(int*)malloc(size_thesi);
-    // i_thesi=(int*)malloc(i_size_thesi);
-    //memset(val,0,(k+1)*sizeof(int));
     for(int i=0;i<=k;i++){
         val[i]=1000000;
-        thesi[i]=1000000;
     }
     
-    // mymap.reserve(k); // RESERVING SPACE BEFOREHAND
-    // mymap.max_load_factor(0.25);
     vector<long int> res;
     bool check_for_1_element=false;
-    int start=0;
     for(int i=0;i<n; i++){
         sum=0;
         //temp_result=999999;
@@ -112,7 +52,6 @@ int main(int argc, char *argv[]){
                 sum+=arr[j];
                 long int v = val[sum];
                 long int len=i-j;
-                // long int th=thesi[sum];
                 // printf("[%d, %d]aristera sum=%ld with length=%ld",i,j,sum,len);
                 if((v>(i-j) )){
                     val[sum]=i-j;
@@ -122,9 +61,7 @@ int main(int argc, char *argv[]){
                 
             }
             else{
-                start++;
-                // sum=0;
-                // break;
+                break;
             }
         }
         sum=0;
@@ -164,7 +101,7 @@ int main(int argc, char *argv[]){
             }
             else  {
             
-                
+                break;
             }
             
 
@@ -180,11 +117,4 @@ int main(int argc, char *argv[]){
     }
 
     cout<<result<<endl;
-    // auto end = std::chrono::high_resolution_clock::now();
-    // auto duration_pref=std::chrono::duration_cast<std::chrono::milliseconds>(pref - start);
-    // auto duration=std::chrono::duration_cast<std::chrono::milliseconds>(after_reading - start);
-    // auto duration_end=std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    // cout<<"after reading "<<duration.count() << endl;
-    // cout<<"pref "<<duration_pref.count() << endl;    
-    // cout<<"duration "<<duration_end.count() << endl;
 }
